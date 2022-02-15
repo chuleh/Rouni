d7495fc1e69ee917211e00cf2c8c00cdb9be86dc
chuleh <lcasarettom@gmail.com>
2022-02-15 18:28:36 -0300

    feat: rouni initial release

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 Rouni: el bot más cajetilla de todo Discord
 '''
 
-import os, asyncio, random, youtube_dl, discord
+import os 
+import asyncio
+import random
+import youtube_dl 
+import discord
 from discord.ext import commands, tasks
 from pkg.cogs.sounds import Sounds
 from dotenv import load_dotenv
@@ -11,11 +15,8 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-# client
-client = discord.Client()
-
 # create bot
-ronald = commands.Bot(command_prefix='!r', subcommand=None)
+ronald = commands.Bot(command_prefix='!r ', help_comm=None)
 
 # sound cog
 ronald.add_cog(Sounds(ronald))
@@ -23,19 +24,38 @@ ronald.add_cog(Sounds(ronald))
 # main
 @ronald.event
 async def on_ready():
-    print('Ronald has become self-aware')
+    print("ready")
 
-@ronald.command
-async def join(ctx):
-    if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else:
-        await ctx.send('{0.user} is full ronald'.format(client))
 
 @ronald.command()
 async def casquet(ctx):
     for client in ronald.voice_clients:
         await client.disconnect(force=True)
+
+@ronald.command()
+async def status(ctx):
+    channel = ronald.get_channel(727307693924286517)
+    await channel.send('Ronald is self aware')
+
+@ronald.command()
+async def stop (ctx):
+    channel = ronald.get_channel(727307693924286517)
+    await channel.send('Ronald does not compute')
+    
+
+@ronald.command()
+async def ayuda(ctx, subhelp=None):
+    if subhelp == 'newclip':
+        await ctx.send('```!r newclip => crea un nuevo clip basado en youtube.\n'
+                        '   ej: !r newclip youtube.com/xyz 00:01:20 05\n'
+                        '       crea un clip de 1 min 20 segundos que dura 5 segundos de largo.\n'
+                        '  !r saveclip <nombre>\n'
+                        '       guarda el clip recién creado con <nombre>.```')
+    else:
+        await ctx.send('```Comandos: \n'
+                       '!r play => rouni goes full ronald y tira sonidos random en tiempo random.\n'
+                       '!r newclip => crea un nuevo clip "!r ayuda newclip" para más info.\n'
+                       '!r saveclip <nombre> => guarda el clip recién creado con su <nombre>```')
+
 
 ronald.run(os.environ['DISCORD_BOT_TOKEN'])
--- pkg/cogs/sounds.py
+++ pkg/cogs/sounds.py
@@ -1,27 +1,37 @@
-import asyncio, os, random, re, shutil, discord
+import asyncio
+import os
+import random
+import re
+import shutil
+import discord
+
 from fileinput import filename 
 from discord.ext import commands
+from pkg.utils.config import cfg
 
 import pkg.utils.aws_utils as aws
-
-from pkg.utils.config import cfg
 
 RESOURCE_PATH = 'resources/sounds/'
 
 # rouni plays sounds
 class Sounds(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, rouni):
+        self.rouni = rouni
         if cfg['use_aws_resources']:
             aws.sync_resources(cfg['bucket_name'], cfg['sounds_prefix'], RESOURCE_PATH)
+
 
     @commands.command()
     async def play(self, ctx, *, sound='random'):
         user = ctx.message.author
+        # user not in voice channel
+        if user.voice is None:
+            await ctx.send("Ronald does not compute")
         # only play sound if user is in a voice channel
-        if user.voice is not None:
+        elif user.voice is not None:
             all_sounds = get_clips()
             channel = user.voice.channel
+
             if sound == 'random':
                 random_sound = random.choice(all_sounds)
                 await play_clip(channel, get_clip_file(random_sound))
@@ -31,11 +41,11 @@ class Sounds(commands.Cog):
     @commands.command()
     async def resync(self,ctx):
         if cfg['use_aws_resources']:
-            await ctx.send("Rouni has become self aware: resyncing")
+            await ctx.send("Ronald has become self aware: loading the big guns")
             if os.path.exists(RESOURCE_PATH):
                 shutil.rmtree(RESOURCE_PATH)
             aws.sync_resources(cfg['bucket_name'], cfg['sounds_prefix'], RESOURCE_PATH)
-            await ctx.send("Rouni has become self aware: resynced")
+            await ctx.send("Ronald has become self aware: lock and loaded")
 
 # defs
 async def play_clip(channel, sound_file):
